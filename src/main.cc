1d9eec9ff5ba7911436f68122452f70edcde4a24
Robert Norlander <robert.norlander@gmail.com>
2012-02-04 22:51:40 +0100

    Added some dummy implementation of main()

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,19 @@
 #include <iostream>
+#include "DummySearch.hpp"
 
 int main(int argc, char *argv[])
 {
 	std::cout << "yad" << std::endl;
+
+	ISearchable *searchable;
+
+	// TODO: Check what site we should search from
+
+	searchable = new DummySearch();
+	searchable->search(argv[1]);
+
+	std::cout << "Result: " << std::endl <<
+		searchable->getResult() << std::endl;
+
+	delete searchable;
 }
