8b589b5b496c33a6720693efe9718a08487c0ade
Erik <you@example.com>
2012-03-21 02:47:14 +0100

    Added arguments in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,66 @@
 #include <iostream>
 #include "NetworkConnection.hpp"
 #include "TydaSearch.hpp"
+#include "DummySearch.hpp"
+
 
 int main(int argc, char *argv[])
 {
-	std::cout << "yad" << std::endl;
 
-	ISearchable *searchable;
+    ISearchable *searchable;
 
 	// TODO: Check what site we should search from
+    if (argc == 2){
+        //TODO Standard search
+        //TODO: should we really have dynamic allocation???
+        searchable = new TydaSearch();
+        searchable->search(argv[1]);
+        std::cout << searchable->getResult() << std::endl;
+        delete searchable;
 
-	searchable = new TydaSearch();
-	searchable->search(argv[1]);
+    }
+    else if (argc == 3){
+        //TODO Check flags for search options
+        if (argv[1][0] != '-'){
+            //TODO error message
+            std::cout << "Error: Too many arguments or no flags!" << std::endl;
+            return 1;
+        }
+        int i = 1;
+        while (argv[1][i] != '\0')
+        {
+            switch(argv[1][i])
+            { //TODO: should we really have dynamic allocation???
 
-	std::cout << searchable->getResult() << std::endl;
+                case 't' : searchable = new TydaSearch();
+                           break;
+                case 'd' : searchable = new DummySearch();
+                           break;
+                default : //TODO error message
+                          std::cout << "Error: Flag " 
+                              << "\"" << argv[1][i] << "\""
+                              << " dose not exist!" << std::endl;
+                          return 1;
+            }
+            std::cout << "flag: " << argv[1][i] << " word: " << argv[2] << std::endl;
 
-	delete searchable;
+            searchable->search(argv[2]);
+            std::cout << searchable->getResult() << std::endl;
+            delete searchable;
+            i++;
+        }
+        if (i == 1){
+            //TODO error message
+            std::cout << "Error: No flags!" << std::endl;
+        }
+    }
+    else
+    {
+        //TODO error message
+        std::cout << "Error: Too many or no arguments!" << std::endl;
+        return 1;
+    }
+
+
+    return 0;
 }
