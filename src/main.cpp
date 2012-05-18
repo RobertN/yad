29b3763f037bd3eb5be23a2b82deaf45c31af613
Robert Norlander <robert.norlander@gmail.com>
2012-05-18 19:08:59 +0200

    Fixed indentation in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,63 +7,66 @@
 int main(int argc, char *argv[])
 {
 
-    ISearchable *searchable;
+	ISearchable *searchable;
 
 	// TODO: Check what site we should search from
-    if (argc == 2)
-    {
-        //TODO Standard search
-        //TODO: should we really have dynamic allocation???
-        searchable = new TydaSearch();
-        searchable->search(argv[1]);
-        std::cout << searchable->getResult() << std::endl;
-        delete searchable;
+	if (argc == 2)
+	{
+		//TODO Standard search
+		//TODO: should we really have dynamic allocation???
+		searchable = new TydaSearch();
+		searchable->search(argv[1]);
+		std::cout << searchable->getResult() << std::endl;
+		delete searchable;
+	}
+	else if (argc == 3)
+	{
+		//TODO Check flags for search options
+		if (argv[1][0] != '-')
+		{
+			//TODO error message
+			std::cout << "Error: Too many arguments or no flags!" << std::endl;
+			return 1;
+		}
+		int i = 1;
+		while (argv[1][i] != '\0')
+		{
+			switch(argv[1][i])
+			{ //TODO: should we really have dynamic allocation???
 
-    }
-    else if (argc == 3)
-    {
-        //TODO Check flags for search options
-        if (argv[1][0] != '-')
-        {
-            //TODO error message
-            std::cout << "Error: Too many arguments or no flags!" << std::endl;
-            return 1;
-        }
-        int i = 1;
-        while (argv[1][i] != '\0')
-        {
-            switch(argv[1][i])
-            { //TODO: should we really have dynamic allocation???
+				case 't' : searchable = new TydaSearch();
+					   break;
+				case 'd' : searchable = new DummySearch();
+					   break;
+				default : //TODO error message
+					   std::cout << "Error: Flag "
+						   << "\"" << argv[1][i] << "\""
+						   << " dose not exist!" << std::endl;
+					   return 1;
+			}
+			std::cout << "flag: " << argv[1][i] << " word: " << argv[2] << std::endl;
 
-                case 't' : searchable = new TydaSearch();
-                           break;
-                case 'd' : searchable = new DummySearch();
-                           break;
-                default : //TODO error message
-                          std::cout << "Error: Flag "
-                              << "\"" << argv[1][i] << "\""
-                              << " dose not exist!" << std::endl;
-                          return 1;
-            }
-            std::cout << "flag: " << argv[1][i] << " word: " << argv[2] << std::endl;
+			std::cout << "Searching" << std::endl;
+			searchable->search(argv[2]);
+			std::cout << "Retrieving results" << std::endl;
+			std::cout << searchable->getResult() << std::endl;
+			std::cout << "Deleting searchable" << std::endl;
+			delete searchable;
+			std::cout << "Done" << std::endl;
+			i++;
+		}
+		if (i == 1)
+		{
+			//TODO error message
+			std::cout << "Error: No flags!" << std::endl;
+		}
+	}
+	else
+	{
+		//TODO error message
+		std::cout << "Error: Too many or no arguments!" << std::endl;
+		return 1;
+	}
 
-            searchable->search(argv[2]);
-            std::cout << searchable->getResult() << std::endl;
-            delete searchable;
-            i++;
-        }
-        if (i == 1)
-        {
-            //TODO error message
-            std::cout << "Error: No flags!" << std::endl;
-        }
-    }
-    else
-    {
-        //TODO error message
-        std::cout << "Error: Too many or no arguments!" << std::endl;
-        return 1;
-    }
-
-    return 0;
+	return 0;
 }
