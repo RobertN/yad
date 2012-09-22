213e0fede96a4890f29273550532ab5a7216d180
Robert Norlander <robert.norlander@gmail.com>
2012-09-23 00:40:53 +0200

    Fix indentation in ResultBuilder.hpp

--- src/ResultBuilder.hpp
+++ src/ResultBuilder.hpp
@@ -12,38 +12,38 @@ typedef pair< int, string > ResultEntry;
 class ResultBuilder
 {
 public:
-    /**
-     * Generates a string from the results that has been
-     * added to the builder.
-     */
+	/**
+	 * Generates a string from the results that has been
+	 * added to the builder.
+	 */
 	string getResultString() const;
 
-    /**
-     * Adds a new result to the builder.
-     *
-     * @param type Defines what the type of the result is.
-     * @param value The value for the result.
-     */
+	/**
+	 * Adds a new result to the builder.
+	 *
+	 * @param type Defines what the type of the result is.
+	 * @param value The value for the result.
+	 */
 	void addResult(int type, const string& value);
 
-    /**
-     * Removes all results.
-     */
+	/**
+	 * Removes all results.
+	 */
 	void clear() { m_entries.clear(); }
 
-    /**
-     * Since a search can generate different kinds of responses
-     * the user of the ResultBuilder can create a new type for
-     * each kind of response that should be saved.
-     *
-     * @param description Description for the new response type.
-     * @return Returns ID for the newly created response type.
-     */
+	/**
+	 * Since a search can generate different kinds of responses
+	 * the user of the ResultBuilder can create a new type for
+	 * each kind of response that should be saved.
+	 *
+	 * @param description Description for the new response type.
+	 * @return Returns ID for the newly created response type.
+	 */
 	int addResultType(const string& description);
 
 private:
 	vector< ResultEntry > m_entries;
-    vector< string > m_result_types;
+	vector< string > m_result_types;
 };
 
 #endif // RESULTBUILDER_HPP
