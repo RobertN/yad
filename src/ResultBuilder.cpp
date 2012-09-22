2ff8ed843fcc05d43664554a78136348653381ad
Robert Norlander <robert.norlander@gmail.com>
2012-09-23 00:32:03 +0200

    Fix indentation in ResultBuilder.cpp

--- src/ResultBuilder.cpp
+++ src/ResultBuilder.cpp
@@ -12,26 +12,26 @@ string ResultBuilder::getResultString() const
 	vector< ResultEntry >::const_iterator it = m_entries.begin();
 	stringstream result_stream;
 
-    bool first = true;
-    for (int i = 0; i < m_result_types.size(); i++)
-    {
-        result_stream << m_result_types[i] << ": ";
-        for (it = m_entries.begin(); it != m_entries.end(); it++)
-        {
-            if ((*it).first == i)
-            {
-                if (first)
-                {
-                    result_stream << " " << (*it).second;
-                    first = false;
-                }
-                else
-                    result_stream << ", " << (*it).second;
-            }
-        }
-        result_stream << endl;
-        first = true;
-    }
+	bool first = true;
+	for (int i = 0; i < m_result_types.size(); i++)
+	{
+		result_stream << m_result_types[i] << ": ";
+		for (it = m_entries.begin(); it != m_entries.end(); it++)
+		{
+			if ((*it).first == i)
+			{
+				if (first)
+				{
+					result_stream << " " << (*it).second;
+					first = false;
+				}
+				else
+					result_stream << ", " << (*it).second;
+			}
+		}
+		result_stream << endl;
+		first = true;
+	}
 	return result_stream.str();
 }
 
@@ -42,6 +42,6 @@ void ResultBuilder::addResult(int type, const string& value)
 
 int ResultBuilder::addResultType(const string& description)
 {
-    m_result_types.push_back(description);
-    return m_result_types.size() - 1;
+	m_result_types.push_back(description);
+	return m_result_types.size() - 1;
 }
